ac503e248f826b30d6b030f91755b4535c0e777b
browndog2408 <michael.brown@impact-tech.com>
2015-06-16 11:42:47 -0400

    update
    update

--- server_setup/recipes/install_gem_services.rb
+++ server_setup/recipes/install_gem_services.rb
@@ -1,6 +1,6 @@
 powershell_script 'Install_Gem_Services' do
   code <<-EOH
-     cd "C:\opscode\chef\embedded\bin\"
+     cd "C:\\opscode\\chef\\embedded\\bin"
 	 ./gem install win32-open3 ruby-wmi windows-api windows-pr --no-rdoc --no-ri --verbose
 	 ./gem install rdp-ruby-wmi
   EOH
