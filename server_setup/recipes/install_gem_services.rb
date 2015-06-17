7fb9874ab8c4fde003086690be598201c801d9ad
browndog2408 <michael.brown@impact-tech.com>
2015-06-17 14:38:20 -0400

    update
    update

--- server_setup/recipes/install_gem_services.rb
+++ server_setup/recipes/install_gem_services.rb
@@ -2,3 +2,18 @@
 gem_package "rdp-ruby-wmi" do
   action :install
 end
+gem_package "win32-open3" do
+  action :install
+end
+gem_package "ruby-wmi" do
+  action :install
+end
+gem_package "windows-api" do
+  action :install
+end
+gem_package "windows-pr" do
+  action :install
+end
+
+
+
