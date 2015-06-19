db5d7d4b7401ac49a0fa74cc57a3cfb0b69c8be2
browndog2408 <michael.brown@impact-tech.com>
2015-06-19 13:05:30 -0400

    update

--- server_setup/recipes/install_windowsupdates.rb
+++ server_setup/recipes/install_windowsupdates.rb
@@ -1,7 +1,7 @@
 powershell_script 'Install_WindowsUpdates' do
   code <<-EOH
-     ipmo "C:\chef\cookbooks\server_setup\files\PSWindowsUpdate"
-	 get-wuinstall -acceptall -confirm -ignorereboot
+     import-module "C:\chef\cookbooks\server_setup\files\PSWindowsUpdate"
+	 # get-wuinstall -acceptall -confirm -ignorereboot
   EOH
   guard_interpreter :powershell_script
   
