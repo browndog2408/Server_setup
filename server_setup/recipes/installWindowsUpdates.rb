500c05e7a18095e3a105a30445892893826e0f8e
browndog2408 <michael.brown@impact-tech.com>
2015-06-19 13:28:55 -0400

    update
    update

--- server_setup/recipes/installWindowsUpdates.rb
+++ server_setup/recipes/installWindowsUpdates.rb
@@ -1,5 +1,5 @@
 powershell_script 'installWindowsUpdates' do
-  code 'import-module "C:\chef\cookbooks\server_setup\files\PSWindowsUpdate" ;get-wuinstall -acceptall -confirm -ignorereboot'
+  code 'import-module "C:\chef\cookbooks\server_setup\files\PSWindowsUpdate"'
   guard_interpreter :powershell_script
   
 end
